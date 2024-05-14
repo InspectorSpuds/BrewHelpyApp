1f45fccbc47aeabade9a6e25be359cf9ea3191df
InspectorSpuds <ishanparikh18@gmail.com>
2024-05-13 22:43:20 -0700

    Merged Kevin Bui's changes from new-recipe-form: unit testing and brew steps Added brew timer support

--- lib/brew_timer.dart
+++ lib/brew_timer.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class BrewTimer extends StatefulWidget {
@@ -11,9 +14,87 @@ class BrewTimer extends StatefulWidget {
 
 class _BrewTimerState extends State<BrewTimer> {
   //timer variables
+  Timer? timer;
+  List<Map<String, dynamic>> steps = [
+    //note: the timestamps are when to add the mass by
+    {
+      "mass" : 50,
+      "timestamp" : "0:30",
+    },
+    {
+      "mass" : 100,
+      "timestamp": "0:00"
+    }
+  ];
+  int nextMinutes = 0;
+  int nextSeconds = 0;
+
+  int stopMinutes = 0;
+  int stopSeconds = 0;
+
+  //ui vars
   int step = 0;
-  String messageBoard = "Starting timer";
-  Duration duration = Duration(minutes: 2, seconds: 2);
+  String messageBoard = "Add mass";
+  int minutes = 0;
+  int seconds = 0;
+
+  void startTimer() {
+    //get the final conditions
+    step = 0;
+    stopMinutes = int.parse(steps[steps.length - 1]['timestamp'].split(":")[0]);
+    stopSeconds = int.parse(steps[steps.length - 1]['timestamp'].split(":")[1]);
+
+    //get next goalpost if exists
+    nextMinutes = int.parse(steps[step]['timestamp'].split(":")[0]);
+    nextSeconds = int.parse(steps[step]['timestamp'].split(":")[1]);
+    messageBoard = "Add to ${steps[step]['mass']}g by ${steps[step]['timestamp']}";
+
+    print("$nextMinutes : $nextSeconds");
+
+    //set periodic time
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTimer());
+  }
+
+  void stopTimer() {
+    timer?.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+    });
+  }
+
+  void addTimer() {
+    setState(() {
+      seconds++;
+
+      if(seconds >= 60) {
+        seconds = 0;
+        minutes++;
+      }
+
+      //if step post reached
+      if(minutes == nextMinutes && seconds == nextSeconds) {
+        moveToNextStep();
+      }
+
+      //stop timer at final step
+      if(minutes == stopMinutes && seconds == stopSeconds + 1) {
+        messageBoard = "You finished the brew!";
+        stopTimer();
+      }
+    });
+  }
+
+  void moveToNextStep() {
+    step++;
+
+    //only update if more steps left
+    if(step < steps.length) {
+      nextMinutes = int.parse(steps[step]['timestamp'].split(":")[0]);
+      nextSeconds = int.parse(steps[step]['timestamp'].split(":")[1]);
+      messageBoard = "Add to ${steps[step]['mass']}g by ${steps[step]['timestamp']}";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +102,43 @@ class _BrewTimerState extends State<BrewTimer> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Step $step: $messageBoard"),
-          TextButton(
-              onPressed: () {
-
-              },
-              child: const Text("Start"),
+          //The next step
+          Text(messageBoard, style: const TextStyle(fontSize: 25),),
+          const SizedBox(
+            height: 50,
           ),
+          //the timer board
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("$minutes", style: const TextStyle(fontSize: 25)),
+                const Text(":", style: TextStyle(fontSize: 25)),
+                Text("$seconds", style: const TextStyle(fontSize: 25)),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  startTimer();
+                },
+                child: const Text("Start"),
+              ),
+              TextButton(
+                  onPressed: () {
+                    stopTimer();
+                  },
+                  child: const Text("Stop")
+              )
+            ],
+          )
+
         ]
       )
     );
