af53c9cc44743d87867572154cbcd87c97f4616c
InspectorSpuds <ishanparikh18@gmail.com>
2024-05-27 17:48:03 -0700

    Added icon assets and preferences

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -4,12 +4,19 @@ class AppDetails extends ChangeNotifier {
   int currPage = 0;
   String recipeKey = "";
 
+  bool enablePushNotfications = false;
+
   void updatePage(int page) {
     currPage = page;
     notifyListeners();
   }
   void updateRecipe(String key) {
     recipeKey = key;
+    notifyListeners();
+  }
+
+  void updatePushNotificationPref(bool pref) {
+    enablePushNotfications = pref;
     notifyListeners();
   }
 }
